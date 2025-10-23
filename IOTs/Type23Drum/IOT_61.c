6ffc7c34254fa3d155c7b0997a2c43dca28d8498
wjenh <wje@duckpond.quackers.net>
2025-10-23 15:00:29 -0400

    manual wrong, make it work the way the hardware says it does

--- IOTs/Type23Drum/IOT_61.c
+++ IOTs/Type23Drum/IOT_61.c
@@ -34,6 +34,7 @@ static int writeMode;
 static int ioBusy;
 static int needBreak;
 static int inWait;
+static int goFast;
 static u64 lastSimtime;         // used in the polling code for drumcount updates
 static u64 cmdCompletionTime;   // relative to pdp1P->simtime
 
@@ -50,7 +51,7 @@ iotHandler(PDP1 *pdp1P, int dev, int pulse, int completion)
 {
 int i;
 Word *memBaseP;
-Word readBuf[4096];                 // needed for read/write mode
+Word buffer[4096];                 // needed for read/write mode
 
     if( pulse )
     {
@@ -72,26 +73,38 @@ Word readBuf[4096];                 // needed for read/write mode
     switch( dev )
     {
     case 061:            // dia, drum initial address, in the IO register, or dba, drum break address
+        needBreak = ioBusy = 0;             // just to be sure
+        pdp1P->cksflags &= ~(CKS_DRP | CKS_DRM);    // and not busy or done
+
+        if( pdp1P->mb & 0100 )      // turn on Faster-n-hell mode
+        {
+            goFast = 1;
+            return(1);
+        }
+
+        if( pdp1P->mb & 0200 )      // turn it off
+        {
+            goFast = 0;
+            return(1);
+        }
+
+        readMode = pdp1P->io & 0400000;
+        writeMode = 0;
+        drumAddr = pdp1P->io & 07777;
+        drumReadField = (pdp1P->io >> 12) & 037;
+
+        if( inWait )                    // we don't want to be
+        {
+            inWait = 0;
+            IOCOMPLETE(pdp1P);
+        }
+
         if( pdp1P->mb & 02000 )
         {
             // dba, using the interrupt system. reqiest break
             // The break happens when the drumCount == the drumAddr
             needBreak = 1;
             iotLog("dba, break on %o\n", drumAddr);
-        }
-
-        needBreak = ioBusy = 0;             // just to be sure
-        pdp1P->cksflags &= ~(CKS_DRP | CKS_DRM);    // and not busy or done
-
-        readMode = pdp1P->io & 0400000;
-        writeMode = 0;
-        drumAddr = pdp1P->io & 07777;
-        drumReadField = (pdp1P->io & 0370000) >> 12;
-
-        if( inWait )                    // we don't want to be
-        {
-            inWait = 0;
-            IOCOMPLETE(pdp1P);
         }
         
         iotLog("dia done, read %d, rfield %d, daddr %d\n", readMode, drumReadField, drumAddr);
@@ -108,8 +121,11 @@ Word readBuf[4096];                 // needed for read/write mode
         {
             writeMode = pdp1P->io & 0400000;
             drumWriteField = (pdp1P->io >> 12) & 037;
-            drumWriteField = (pdp1P->io & 0370000) >> 12;
             transferCount = pdp1P->io & 07777;
+            if( !transferCount )
+            {
+                transferCount = 4096;       // 0 means entire track
+            }
 
             iotLog("dwc done, write %d, wfield %d, count %o\n", writeMode, drumWriteField, transferCount);
             pdp1P->cksflags &= ~CKS_DRM;    // not done now
@@ -123,40 +139,44 @@ Word readBuf[4096];                 // needed for read/write mode
         break;
 
     case 063:            // dcl, drum core location
-        memBank = (pdp1P->io >> 14) & 03;
-        memAddr = pdp1P->io & 017777;
+        // The manual says bits 2, 3, but this isn't correct.
+        // The hardware description is.
+        // It's adtually bits 2-5 to support up to 16 memory modules.
+        memBank = (pdp1P->io >> 12) & 037;      // support large memory -1's
+        memAddr = pdp1P->io & 07777;
 
         iotLog("dcl memBank %o memAddr %o\n", memBank, memAddr);
 
-        memBaseP = &pdp1P->core[(memBank % 037) * 4096];
+        memBaseP = &pdp1P->core[memBank * 4096];
 
-        // and away we go
-        // We might have read/write mode, in which case we have to read the data first
+        // And away we go.
+        // For read-write mode, we read data first, then write.
+        // This is the sequence defined in the hardware description.
+        // Both the drum address and the memory address can wrap around.
         // Both the drum address and the memory address can wrap around
-
         if( readMode && writeMode )
         {
             // First we read the drum, but don't put it in memory yet
-            // we need 2 buffer, though
-            readDrumToBuffer(drumFd, readBuf, drumReadField, drumAddr, transferCount);
+            // we need 2 buffers, though
+            readDrumToBuffer(drumFd, buffer, drumReadField, drumAddr, transferCount);
             Word writeBuf[4096];
             copyMemToBuffer(writeBuf, memBaseP, memAddr, transferCount);
             writeBufferToDrum(drumFd, writeBuf, drumWriteField, drumAddr, transferCount);
 
             // Now we can overwrite mem with the drum data
 
-            copyBufferToMem(readBuf, memBaseP, memAddr, transferCount);
+            copyBufferToMem(buffer, memBaseP, memAddr, transferCount);
         }
         else if( readMode )            // just a regular read
         {
-            readDrumToBuffer(drumFd, readBuf, drumReadField, drumAddr, transferCount);
-            copyBufferToMem(readBuf, memBaseP, memAddr, transferCount);
+            readDrumToBuffer(drumFd, buffer, drumReadField, drumAddr, transferCount);
+            copyBufferToMem(buffer, memBaseP, memAddr, transferCount);
         }
         else if( writeMode )
         {
             // We can use readbuf for this
-            copyMemToBuffer(readBuf, memBaseP, memAddr, transferCount);
-            writeBufferToDrum(drumFd, readBuf, drumWriteField, drumAddr, transferCount);
+            copyMemToBuffer(buffer, memBaseP, memAddr, transferCount);
+            writeBufferToDrum(drumFd, buffer, drumWriteField, drumAddr, transferCount);
         }
         else
         {
@@ -199,7 +219,9 @@ iotStart()
         drumFd = open(DRUMFILE, O_RDWR + O_CREAT + O_SYNC, 0666);
         iotLog("IOT 61 drumFd = %d\n", drumFd);
     }
+
     needBreak = 0;
+    drumCount = 0;  // we don't really know where the hardware would have been, just use 0
 }
 
 void
@@ -220,12 +242,12 @@ iotPoll(PDP1 *pdp1P)
 {
     if( ioBusy )
     {
-        if( pdp1P->simtime >= cmdCompletionTime )
+        if( goFast || (pdp1P->simtime >= cmdCompletionTime) )
         {
             ioBusy = 0;
             pdp1P->cksflags |= CKS_DRM;     // done
             pdp1P->cksflags &= ~CKS_DRP;    // and not busy
-            drumCount = drumAddr + transferCount;   // sync up the drum count to match the end of the transfer
+            drumCount = (drumAddr + transferCount) % 4096;   // sync up the drum count to match the end of the transfer
 
             if( inWait )
             {
@@ -243,7 +265,7 @@ iotPoll(PDP1 *pdp1P)
         // This won't be exact, but the longer the time but the higher the count, the more accurate it will be.
         // The worst case will be a 10us interval.
 
-        if( pdp1P->simtime >= (lastSimtime + 8500) )
+        if( goFast || (pdp1P->simtime >= (lastSimtime + 8500)) )
         {
             lastSimtime = pdp1P->simtime;
             drumCount = ++drumCount % 4096;
@@ -254,7 +276,7 @@ iotPoll(PDP1 *pdp1P)
             ioBusy = needBreak = 0;
             pdp1P->cksflags |= CKS_DRM;     // done
             pdp1P->cksflags &= ~CKS_DRP;    // and not busy
-            initiateBreak(0);           // no channel specified, what to use for sbs16? Is 0 correct??
+            initiateBreak(5);               // the DEC drum diagnostic seems to use channel 5
             iotLog("IOT 61 break initiated at drum count %o.\n", drumCount);
         }
     }
--- IOTs/Type23Drum/README.txt
+++ IOTs/Type23Drum/README.txt
@@ -1,11 +1,22 @@
 This is an implementation of the Type 23 Drum memory.
 Just copy to the IOTs directory and make in order to use it.
 
+IMPORTANT - The H23 Paralell Drum Jul 64 manual has some very signifiant errors, don't believe it all.
+The opcode for DCL is totally wrong.
+The memory field is actually bits 1-5 to support the full extened memory possible.
+The address field is as you would expect, 6-17.
+The opcode for DRA is 722062, NOT 723062. Using the latter will hang the -1.
+The listing for the diagnostic routine is missing some defines and has several typos and errors.
+
 It simulates the timing of the real drum, more or less.
 Transfer times will be correct, 8.5 us per word transferred.
 The drum count, 0-4095, which is the current word location for the drum, is not timed exactly.
 The real drum increments the count once every 8.5 us, but the emulator calls the poll method once ever 5 us.
 So, the count increments faster than the original. This might affect the timing of some programs.
+
+However, if you don't want to twiddle your thumbs for many milliseconds, there are 2 nonstandard additons.
+IOT 161, DIA 100, will turn on fast mode, operations will complete immediately.
+IOT 261, DIA 200, turns that off.
 
 A transfer request is started immediately on execution of dcl, but the completion will appear to be delayed for
 the proper time, 8.5 us per drum location needed to reach the start address plus 8.5 us per word transferred.
@@ -15,6 +26,14 @@ When started, it will execute multiple tests, halting after each one.
 The program flags will show the test number, and the values in the AC and IO registers should match.
 Press continue to go on to the next test.
 When all tests have been done, the AC and IO will be zero and program flags will be off.
+
+Also included is the DEC-1-137M diagnostic test program painfully copied and fixed from the manual.
+Set the test switches to 02000 before you load it. That selects mem bank 2 for the test to use.
+No output on the typewriter is a good sign.  It will eventually finish.
+As it was, the test never ends, it loops forever on the final test. This is less than useful.
+The code has been changed to print a done message and halt.
+And yes, the implementation passes. Note that the break system test requires the sbs16 break system which needs
+dynamic IOT_60 to enable.
 
 drumloader.mac is a special loader that can save a core image to drum and later restore it.
 It resides in the same space as the default bin loader, except a few words bigger.

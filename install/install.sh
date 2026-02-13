c7e2aa3f69230ccf2d6edae912c4ff1c147bb749
wjenh <wje@duckpond.quackers.net>
2026-02-13 08:27:16 -0500

    update gitignore, delete some now-ignored files

--- install/install.sh
+++ install/install.sh
@@ -82,28 +82,14 @@ while true; do
 	    # telnet is no longer part of standard linux...
 	    sudo apt install telnet
 
+            # needed to build am1
+	    sudo apt -y install flex
+	    sudo apt -y install bison
+
 	    break
 	    ;;
         [Nn]* ) 
             echo Skipped install of dependencies - OK if you installed them already
-            break
-	    ;;
-        * ) echo "Please answer Y or N.";;
-    esac
-done
-
-while true; do
-    echo
-    read -p "Install flex and bison - required for building some of the tools? " prxn
-    case $prxn in
-        [Yy]* ) 
-            sudo apt update
-	    sudo apt -y install flex
-	    sudo apt -y install bison
-	    break
-	    ;;
-        [Nn]* ) 
-            echo Skipped install of flex and bison - some tools will not build unless these are already installed
             break
 	    ;;
         * ) echo "Please answer Y or N.";;

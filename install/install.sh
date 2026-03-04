118ba088e74330573f15e0a76bed3732cb7aa0ec
wjenh <wje@duckpond.quackers.net>
2026-03-04 11:13:10 -0500

    push the actually updated install.sh

--- install/install.sh
+++ install/install.sh
@@ -187,7 +187,7 @@ while true; do
 		make -C $INSTALLDIR/src/usb_paper_tape
 		# The tape disassembler
                 make -C $INSTALLDIR/Tools/Disassembler
-		cp $INSTALLDIR/Tools/Disassembler/disassemble_tape $INSTALLDIR/bin
+		cp $INSTALLDIR/Tools/Disassembler/disassembler $INSTALLDIR/bin/disassemble_tape`
 		# The etended audio control
 		make -C $INSTALLDIR/Tools install
             

9bf7b300d638603c0709a2676b549ba486c9e10a
wjenh <wje@duckpond.quackers.net>
2026-05-15 12:57:46 -0400

    update AD1 for new include files

--- Tools/AD1/pdp1inc.h
+++ Tools/AD1/pdp1inc.h
@@ -6,13 +6,6 @@ typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint16_t u16;
 
-typedef struct FD
-{
-    int fd;
-    int ready;
-    int id;
-} FD;
-
 // We need this to pick up the definition of the PDP1 data structure.
 #define USEAM1
 #include "/opt/pidp1-mods/src/blincolnlights/pdp1/pdp1.h"
